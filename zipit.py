46fee04cbe664454a2695d42fa661008c5a97016
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2015-09-02 17:50:49 -0600

    test

--- zipit.py
+++ zipit.py
@@ -110,5 +110,5 @@ app.add_route('/compress', zip_it)
 
 
 if __name__ == '__main__':
-    httpd = simple_server.make_server('127.0.0.1', 8000, app)
+    httpd = simple_server.make_server('0.0.0.0', 8000, app)
     httpd.serve_forever()

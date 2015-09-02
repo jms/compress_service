7d664b7099ae198a0892453512f51a893dafde4c
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2015-09-02 11:59:32 -0600

    test without nginx

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ sudo pip install -r requirements.txt
 sudo cp -v  $HOME/compress_service/zipit.conf /etc/supervisor/conf.d/
 sudo cp -v  $HOME/compress_service/rqworker.conf /etc/supervisor/conf.d/
 
-sudo cp -v  $HOME/compress_service/zipit-nginx.conf /etc/nginx/conf.d/
+# sudo cp -v  $HOME/compress_service/zipit-nginx.conf /etc/nginx/conf.d/
 
 sudo service nginx reload 
 sudo service supervisor restart 

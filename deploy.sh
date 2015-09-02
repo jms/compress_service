fb97c9ebdb35eee6ef6356135cb59c1fd826b9eb
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2015-09-02 11:49:07 -0600

    adding service reload/restart

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,8 @@ sudo cp -v  $HOME/compress_service/zipit.conf /etc/supervisor/conf.d/
 sudo cp -v  $HOME/compress_service/rqworker.conf /etc/supervisor/conf.d/
 
 sudo cp -v  $HOME/compress_service/zipit-nginx.conf /etc/nginx/conf.d/
+
+sudo service nginx reload 
+sudo service supervisor restart 
+
+
